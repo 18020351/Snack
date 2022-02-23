dde4f0cc437fdba30099052290468d0ccd5b24b7
ducnv <ducnv.cntt0502@gmail.com>
2022-02-23 12:21:17 +0700

    commit 1

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,25 @@
+
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public BoxCollider2D gridArea;
+    private void Start()
+    {
+        RandomizePosition();
+    }
+    private void RandomizePosition()
+    {
+        Bounds bounds = this.gridArea.bounds;
+        float x = Random.Range(bounds.min.x, bounds.max.y);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+        transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            RandomizePosition();
+        }
+    }
+}
